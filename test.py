f86fb28c84d2cfa7d3597513b417a4f217c35338
theCoderSchool at Corona <155676852+theCoderSchoolCorona@users.noreply.github.com>
2025-12-22 11:41:26 -0800

    finishing dataset

--- test.py
+++ test.py
@@ -6,13 +6,18 @@ df = pd.read_parquet("hf://datasets/batuhanmtl/job-skill-set/data/train-00000-of
 df.to_csv('job-skills.csv')
 
 
+job_description_encoder = sklearn.feature_extraction.text.TfidfVectorizer()
+jobDescription = job_description_encoder.fit_transform(df['job_description'])
+
+job_skill_set_encoder = sklearn.preprocessing.MultiLabelBinarizer()
+jobSkillSet = job_skill_set_encoder.fit_transform(df['job_skill_set'].apply(ast.literal_eval))
+
+job_category_encoder = sklearn.preprocessing.OneHotEncoder(sparse_output=False)
+jobCategory = job_category_encoder.fit_transform(df[['category']])
 
 job_title_encoder = sklearn.feature_extraction.text.TfidfVectorizer()
 y = job_title_encoder.fit_transform(df['job_title'])
 print(y)
 
-job_description_encoder = sklearn.feature_extraction.text.TfidfVectorizer()
-df["job_description_encoded"] = job_description_encoder.fit_transform(df['job_description'])
 
-job_skill_set_encoder = sklearn.preprocessing.MultiLabelBinarizer()
-df["job_skill_set_encoded"] = job_skill_set_encoder.fit_transform(df['job_skill_set'])
+
