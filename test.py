b32d003d0cecea061bac587cd462278da895b8c4
theCoderSchool at Corona <155676852+theCoderSchoolCorona@users.noreply.github.com>
2025-12-22 11:23:21 -0800

    add dataset

--- test.py
+++ test.py
@@ -1,5 +1,18 @@
 import pandas as pd
+import sklearn
+import ast
 
 df = pd.read_parquet("hf://datasets/batuhanmtl/job-skill-set/data/train-00000-of-00001.parquet")
 df.to_csv('job-skills.csv')
 
+
+
+job_title_encoder = sklearn.feature_extraction.text.TfidfVectorizer()
+y = job_title_encoder.fit_transform(df['job_title'])
+print(y)
+
+job_description_encoder = sklearn.feature_extraction.text.TfidfVectorizer()
+df["job_description_encoded"] = job_description_encoder.fit_transform(df['job_description'])
+
+job_skill_set_encoder = sklearn.preprocessing.MultiLabelBinarizer()
+df["job_skill_set_encoded"] = job_skill_set_encoder.fit_transform(df['job_skill_set'])
