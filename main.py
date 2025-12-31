34698effdcc2d1038eedaaa3c9e3e1ea583d3f28
theCoderSchool at Corona <155676852+theCoderSchoolCorona@users.noreply.github.com>
2025-12-31 10:45:50 -0800

    recommend jobs line

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from screen import Screen
 Screen()
 from model import train_autoencoder, generate_job_embeddings, save_model, data_preprocess
 from inf import load_model
+from inf import recommend_jobs
 
 if not os.path.exists("save_dir"):
     df,x,encoders = data_preprocess()
@@ -11,3 +12,7 @@ if not os.path.exists("save_dir"):
     save_model(encoder, encoders, df, job_embeddings)
 else:
     encoder,encoders,job_embeddings,df= load_model()
+
+Screen()
+
+print(recommend_jobs(Screen.Entry4.get(),Screen.Entry3.get(),Screen.Entry2.get(),Screen.Entry1.get(), encoder, encoders, job_embeddings, df, top_n=5))
